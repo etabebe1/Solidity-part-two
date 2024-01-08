da69670d58882be237a289a48623f00dd7e013a4
etabebe1 <etabebe1@gmail.com>
2024-01-08 06:47:29 -0500

    importing enum

---
+++ 25-enumDeclaration.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+enum Status { Pending, Shipping, Rejected, Accepted, Canceled }
---
+++ 26-importing-enum.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./25-enumDeclaration.sol";
+
+contract Enum {
+  Status public status;
+}
